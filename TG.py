b0431ffb274064854abdba640cc4c2e48b868e0a
VictorGarritano <victorgarritano@dcc.ufrj.br>
2017-12-08 17:56:31 -0200

    test

---
+++ TG.py
@@ -0,0 +1,13 @@
+from numpy import *
+fromnumpy.fft import fftfreq,fft,ifft,irfft2,rfft2
+frommpi4py import MPI
+
+nu=0.000625
+T=0.1
+dt=0.01
+N=2**7
+comm=MPI.COMM_WORLD
+num_processes=comm.Get_size()
+rank=comm.Get_rank()
+Np=N/num_processes
+X=mgrid[rank*Np:(rank+1)*Np,:N,:N].astype(float)*2*pi/N
